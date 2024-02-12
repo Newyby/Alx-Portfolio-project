b3a9b5c6c4de3b184a2a96fec6bb79b561a6cf2c
Akinsanmi omobonike <officialraydata@gmail.com>
2024-02-12 19:06:36 +0100

    app.py and second html modified

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
 from flask import Flask, request, send_file
 from PIL import Image
-from io import BytesIO
-
+from io import BytesIO                                        
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return app.send_static_file('second.html')
+                                                              @app.route('/')                                               def index():
+    return app.send_static_file('index.html')
 
 @app.route('/compress', methods=['POST'])
 def compress():
-    if 'image' not in request.files:
-        return "No image provided", 400
-
     image = request.files['image']
-    
-    try:
-        # Open the image using PIL
-        picture = Image.open(image)
-
-        # Compress the image
-        compressed_img_io = BytesIO()
-        picture.save(compressed_img_io, 'JPEG', optimize=True, quality=30)
-        compressed_img_io.seek(0)
-
-        return send_file(compressed_img_io, mimetype='image/jpeg', as_attachment=True, download_name='compressed.jpg')
-    except Exception as e:
-        return f"Error compressing image: {str(e)}", 500
+    picture = Image.open(image)
+    img_io = BytesIO()                                            picture.save(img_io, 'JPEG', optimize=True, quality=30)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='compressed.jpg')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
