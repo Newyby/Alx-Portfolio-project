cf08dbb4c9a27fffe9af7260c752f26fe8c8b239
Akinsanmi omobonike <officialraydata@gmail.com>
2024-02-14 19:03:27 +0100

    app.py modified

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, send_file
 from PIL import Image
-from io import BytesIO                                        
+from io import BytesIO
+
 app = Flask(__name__)
-                                                              @app.route('/')                                               def index():
+
+@app.route('/')
+def index():
     return app.send_static_file('index.html')
 
 @app.route('/compress', methods=['POST'])
 def compress():
     image = request.files['image']
     picture = Image.open(image)
-    img_io = BytesIO()                                            picture.save(img_io, 'JPEG', optimize=True, quality=30)
+    img_io = BytesIO()
+    picture.save(img_io, 'JPEG', optimize=True, quality=30)
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='compressed.jpg')
 
 if __name__ == '__main__':
     app.run()
+
